82d86a8c07fc2928c06c61e89c0bb861191c26df
Ozzie <mikeosmonson@gmail.com>
2021-05-24 14:42:11 -0600

    Add states & capitals hashes.

---
+++ nested_collections_practice.rb
@@ -0,0 +1,15 @@
+states = {
+  "Oregon" => "OR",
+  "Alabama" => "AL",
+  "New Jersey" => "NJ",
+  "Colorado" => "CO"
+}
+capitals = {
+  "OR" => "Salem",
+  "AL" => "Montgomery",
+  "NJ" => "Trenton",
+  "CO" => "Denver"
+}
+
+puts states["Alabama"]
+puts states.capitals["Oregon"]
