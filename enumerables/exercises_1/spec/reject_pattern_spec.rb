d61c352fd28a7b79ed375afa67439a31bfacf8e1
Ozzie <mikeosmonson@gmail.com>
2021-06-22 13:12:02 -0600

    Complete #reject

--- enumerables/exercises_1/spec/reject_pattern_spec.rb
+++ enumerables/exercises_1/spec/reject_pattern_spec.rb
@@ -9,16 +9,17 @@ RSpec.describe 'reject pattern' do
     expect(filtered).to eq([2, 93, 7, 1, 31, 368])
   end
 
-  xit 'removes vowels' do
+  it 'removes vowels' do
     letters = ["a", "l", "l", " ", "y", "o", "u", "r", " ", "b", "a", "s", "e", " ", "a", "r", "e", " ", "b", "e", "l", "o", "n", "g", " ", "t", "o", " ", "u", "s"]
     remaining = []
     letters.each do |letter|
       # Your code goes here
+      remaining << letter unless ["a", "e", "i", "o", "u", "y"].include? letter
     end
     expect(remaining).to eq(["l", "l", " ", "r", " ", "b", "s", " ", "r", " ", "b", "l", "n", "g", " ", "t", " ", "s"])
   end
 
-  xit 'removes numbers divisible by 3' do
+  it 'removes numbers divisible by 3' do
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     remaining = []
     # Your code goes here
