9441f73b5fd7a9e31a14da764758bc77ffb5fe7c
Ozzie <mikeosmonson@gmail.com>
2021-06-24 16:18:51 -0600

    Complete reduce

--- enumerables/exercises_1/spec/reduce_spec.rb
+++ enumerables/exercises_1/spec/reduce_spec.rb
@@ -8,47 +8,65 @@ RSpec.describe 'reduce' do
     expect(result).to eq(473)
   end
 
-  xit 'subtracts a list of numbers' do
+  it 'subtracts a list of numbers' do
     numbers = [28, 12, 38, 1, 91]
     result = numbers.reduce(0) do |difference, number|
       # Your code goes here
+      difference - number
     end
     expect(result).to eq(-170)
   end
 
-  xit 'multiplies a list of numbers' do
+  it 'multiplies a list of numbers' do
     numbers = [2, 3, 5, 7]
     # initial value is 1
+    result = numbers.reduce(1) do |difference, number|
     # Your code goes here
+      difference * number
+    end
+
     expect(result).to eq(210)
   end
 
-  xit 'capitalize key words in phrase' do
+  it 'capitalize key words in phrase' do
     keywords = ["fish", "blue"]
     # initial value is 'one fish two fish red fish blue fish'
+    result = keywords.reduce('one fish two fish red fish blue fish') do |sentence, keyword|
     # Your code goes here
+      sentence.gsub(keyword, keyword.upcase)
+    end
+
     expect(result).to eq('one FISH two FISH red FISH BLUE FISH')
   end
 
-  xit 'divides 560 by a bunch of numbers' do
+  it 'divides 560 by a bunch of numbers' do
     numbers = [2, 2, 2, 5, 7]
     # initial value is 560
+    result = numbers.reduce(560) do |amount, number|
     # Your code goes here
+    amount / number
+  end
     expect(result).to eq(2)
   end
 
-  xit 'subtract smallest values from 100' do
+  it 'subtract smallest values from 100' do
     elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
     # initial value is 100
+    result = elements.reduce(100) do |value, number|
     # Your code goes here
+      value - number.min
+    end
     expect(result).to eq(88)
   end
 
-  xit 'adds all second values together' do
+  it 'adds all second values together' do
     elements = [["a", 1], ["b", 9], ["c", 21]]
     # initial value is 0
+    result = elements.reduce(0) do |value, element|
     # Your code goes here
+      value + element[1]
+    end
+
     expect(result).to eq(31)
   end
 end
-
