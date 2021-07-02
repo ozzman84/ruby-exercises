14deff47032a83f88aa68bd6ae57932db05db3d7
Ozzie <mikeosmonson@gmail.com>
2021-07-02 15:16:58 -0600

    Refactor w/curly brackets

--- enumerables/exercises_1/spec/map_spec.rb
+++ enumerables/exercises_1/spec/map_spec.rb
@@ -12,64 +12,49 @@ RSpec.describe 'map' do
 
   it 'doubles' do
     numbers = [1, 2, 3, 4, 5]
-    doubles = numbers.map do |number|
-      number * 2
-    end
-
+    doubles = numbers.map { |number| number * 2 }
     expect(doubles).to eq([2, 4, 6, 8, 10])
   end
 
   it 'squares' do
     numbers = [1, 2, 3, 4, 5]
-    squares = numbers.map do |number|
-      number * number
-    end
+    squares = numbers.map { |number| number * number }
 
     expect(squares).to eq([1, 4, 9, 16, 25])
   end
 
   it 'lengths' do
     names = ["alice", "bob", "charlie", "david", "eve"]
-    lengths = names.map do |name|
-      name.length
-    end
+    lengths = names.map { |name| name.length }
 
     expect(lengths).to eq([5, 3, 7, 5, 3])
   end
 
-  xit 'normalize zip codes' do
+  it 'normalize zip codes' do
     numbers = [234, 10, 9119, 38881]
-    zip_codes = numbers.map do |number|
-      number.rjust(5, "0")
-    end
+    zip_codes = numbers.map { |num| num.to_s.rjust(5, '0') }
 
     expect(zip_codes).to eq(["00234", "00010", "09119", "38881"])
   end
 
   it 'backwards' do
     names = ["alice", "bob", "charlie", "david", "eve"]
-    backwards = names.map do |name|
-      name.reverse
-    end
+    backwards = names.map { |name| name.reverse }
 
     expect(backwards).to eq(["ecila", "bob", "eilrahc", "divad", "eve"])
   end
 
   it 'words with no vowels' do
     words = ["green", "sheep", "travel", "least", "boat"]
-    without_vowels = words.map do |word|
-      word.delete("aeiou")
-    end
+    without_vowels = words.map { |word| word.delete('aeiou') }
 
     expect(without_vowels).to eq(["grn", "shp", "trvl", "lst", "bt"])
   end
 
   it 'trims last letter' do
     animals = ["dog", "cat", "mouse", "frog", "platypus"]
-    trimmed = animals.map do |animal|
-      animal.chop
-    end
-
+    trimmed = animals.map { |animal| animal.chop }
+    
     expect(trimmed).to eq(["do", "ca", "mous", "fro", "platypu"])
   end
 end
