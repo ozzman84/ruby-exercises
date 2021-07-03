d20fda4625c8c62b0c002765479bb1afd44375f0
Ozzie <mikeosmonson@gmail.com>
2021-07-03 14:31:45 -0600

    Refactor W/{}

--- enumerables/exercises_1/spec/sort_by_spec.rb
+++ enumerables/exercises_1/spec/sort_by_spec.rb
@@ -2,53 +2,47 @@ RSpec.describe 'sort_by' do
 
   it 'alphabetically' do
     words = ["broccoli", "Carrots", "FISH", "Bacon", "candy"]
-    sorted = words.sort_by do |word|
-      word.downcase
-    end
+    sorted = words.sort_by(&:downcase)
+
     expect(sorted).to eq(["Bacon", "broccoli", "candy", "Carrots", "FISH"])
   end
 
   it 'alphabetically by last letter' do
     things = ["pill", "box", "glass", "water", "sponge"]
-    sorted = things.sort_by do |thing|
-      thing[-1]
-    end
+    sorted = things.sort_by { |thing| thing[-1] }
+
     expect(sorted).to eq(["sponge", "pill", "water", "glass", "box"])
   end
 
   it 'distance' do
     distances = ["1cm", "9cm", "30cm", "4cm", "2cm"]
     # Your code goes here
-    sorted = distances.sort_by do |distance|
-      distance.to_i
-    end
+    sorted = distances.sort_by(&:to_i)
+
     expect(sorted).to eq(["1cm", "2cm", "4cm", "9cm", "30cm"])
   end
 
   it 'length' do
     words = ["heteromorph", "ancyloceratina", "bioengineering", "mathematical", "bug"]
     # Your code goes here
-    sorted = words.sort_by do |word|
-      word.length
-    end
+    sorted = words.sort_by(&:length)
+
     expect(sorted).to eq(["bug", "heteromorph", "mathematical", "ancyloceratina", "bioengineering"])
   end
 
   it 'proximity to ten' do
     prices = [3.02, 9.91, 17.9, 10.01, 11.0]
     # Your code goes here
-    sorted = prices.sort_by do |price|
-      (10 - price).abs
-    end
+    sorted = prices.sort_by { |price| (10 - price).abs }
+
     expect(sorted).to eq([10.01, 9.91, 11.0, 3.02, 17.9])
   end
 
   it 'number of cents' do
     prices = [3.02, 9.91, 7.9, 10.01, 11.0]
     # Your code goes here
-    sorted = prices.sort_by do |price|
-      (price - price.round).abs
-    end
+    sorted = prices.sort_by { |price| price - price.to_i }
+    
     expect(sorted).to eq([11.0, 10.01, 3.02, 7.9, 9.91])
   end
 end
